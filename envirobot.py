5b074cbe98caef851aca2a1a374cdb7af1c05249
PJ Evans <pj@mrpjevans.com>
2020-04-14 17:27:55 +0100

    Add particulates

--- envirobot.py
+++ envirobot.py
@@ -30,6 +30,12 @@ try:
 except ImportError:
     import ltr559
 
+# Noise
+from enviroplus.noise import Noise
+
+# Particulates
+from pms5003 import PMS5003, ReadTimeoutError
+
 # Set up logging
 logging.basicConfig(
     format = '%(asctime)s %(levelname)-8s %(message)s',
@@ -39,6 +45,9 @@ logging.basicConfig(
 # Set up sensors
 bus = SMBus(1)
 bme280 = BME280(i2c_dev = bus)
+noise = Noise()
+pms5003 = PMS5003()
+time.sleep(1.0) # Allow particulate sensor to boot
 
 # Settings
 cpu_factor = float(os.getenv("ENVIROPLUS_TEMP_FACTOR"))
@@ -64,21 +73,32 @@ while True:
     avg_cpu_temp = sum(cpu_temps) / float(len(cpu_temps))
     raw_temp = bme280.get_temperature()
     comp_temp = raw_temp - ((avg_cpu_temp - raw_temp) / cpu_factor)
-    logging.info("T:{:04.1f}".format(comp_temp))
-
+    
     # Pressure
     pressure = bme280.get_pressure()
-    logging.info("P:{:05.1f}".format(pressure))
-
+    
     # Humidity
     humidity = bme280.get_humidity()
-    logging.info("H:{:05.1f}".format(humidity))
-
+    
     # Light
     lux = ltr559.get_lux()
 
+    # Noise
+    amp = noise.get_amplitude_at_frequency_range(100, 1200)
+
+    # Gas
+    gas_readings = gas.read_all()
+
+    # Particulates
+    try:
+        pm_data = pms5003.read()
+    except ReadTimeoutError:
+        pms5003 = PMS5003()
+        time.sleep(1)
+        pm_data = pms5003.read()
+
     # Create payload
-    payload = ("{} temp={:04.1f},lux={:05.02f},pressure={:05.02f},humidity={:05.02f}").format(os.getenv("INFLUXDB_MEASUREMENT"), comp_temp, lux, pressure, humidity)
+    payload = ("{} temp={:04.1f},lux={:05.02f},pressure={:05.02f},humidity={:05.02f},amp={:05.04f},gas_oxidising={:05.02f},gas_nh3={:05.02f},gas_reducing={:05.02f},pm_1={},pm25={},pm10={}").format(os.getenv("INFLUXDB_MEASUREMENT"), comp_temp, lux, pressure, humidity, amp, gas_readings.oxidising, gas_readings.nh3, gas_readings.reducing, pm_data.pm_ug_per_m3(1), pm_data.pm_ug_per_m3(2.5), pm_data.pm_ug_per_m3(10))
     logging.info(payload)
 
     # Send to InfluxDB endpoint
